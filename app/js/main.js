e2d3502fd4a342ab373bba19e437669a664de6ee
Hisenzerg <mrhisenberg100@gmail.com>
2021-05-27 19:00:56 -0400

    added filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function () {
   
+  $(".star").rateYo({
+    starWidth: "11px",
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6",
+    readOnly: true,
+    spacing: "9px",
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__items').slick({
     arrows: false,
     dots: true,
