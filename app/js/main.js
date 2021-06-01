991dba95d0b3f1eebcbdae189e5d51b443e0508f
Hisenzerg <mrhisenberg100@gmail.com>
2021-05-31 20:49:25 -0400

    worked on shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,37 @@
 $(function () {
   
-  $(".star").rateYo({
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.products-item').addClass('products-item--list');
+  });
+  
+  $('.button-grid').on('click', function () {
+    $('.products-item').removeClass('products-item--list');
+  });
+  
+  $('.button-list').on('click', function () {
+    $('.shop-content__items').addClass('products-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.shop-content__items').removeClass('products-item--list');
+  });
+
+  $('.select-style').styler()
+
+  $('.star').rateYo({
+    starWidth: "11px",
+    ratedFill: "#ffcc00",
+    normalFill: "#d6d6d6",
+    readOnly: true,
+    spacing: "9px",
+  });
+
+  $('.products-item__star').rateYo({
     starWidth: "11px",
     ratedFill: "#ffcc00",
     normalFill: "#d6d6d6",
